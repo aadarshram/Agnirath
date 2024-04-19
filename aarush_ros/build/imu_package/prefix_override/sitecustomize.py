fd787318a53e46f359ef83213145457e23971d48
JadeVexo <jaayanth.jr@gmail.com>
2024-04-19 10:56:38 +0530

    deletion

--- aarush_ros/build/imu_package/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jaay/Agnirath/Agnirath_LVS_Strategy/aarush_ros/install/imu_package'
--- aarush_ros/install/can_package/lib/python3.10/site-packages/can_package/py_tools_test.py
+++
@@ -1,9 +0,0 @@
-import cantools
-from pprint import pprint
-#f =  open("~/Agnirath_LVS_Strategy/aarush_ros/src/can_package/can_package/DBC_Files/mc_can.dbc",)
-db = cantools.database.load_file('/home/ubuntu/Agnirath_LVS_Strategy/aarush_ros/src/can_package/can_package/DBC_Files/mc_can.dbc')
-db.messages
-while True:
-
-	example_message = db.get_message_by_name('Bus_Measurement')
-	pprint(example_message.signals)
